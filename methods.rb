2341493b3e438f2718db4a281897f6fab319e52f
Tunisia Artope <artope@yahoo.com>
2022-03-12 17:51:43 -0500

    commit tree a83c4ede082824d11dc5457b4ebbbb540b9d044b

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+# def halve(number)
+#     return nil unless number.class == Integer
+    
+#     number / 2
+# end
+
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+    number / 2
+end
